6e457ca16891dd014fc3dd474b4826ba1b648244
Aaron Chafetz <achafetz@usaid.gov>
2021-04-28 09:15:51 -0400

    new input, convert na to 0

--- Scripts/COP21_TZA_HTS_OtherPITC_allocation-adj.R
+++ Scripts/COP21_TZA_HTS_OtherPITC_allocation-adj.R
@@ -3,17 +3,22 @@
 # PURPOSE:  adjust issues due to rounding on HTS tab
 # LICENSE:  MIT
 # DATE:     2021-04-20
-# UPDATED: 
+# UPDATED:  2021-04-28
 
 # DEPENDENCIES ------------------------------------------------------------
   
   library(tidyverse)
   library(readxl)
 
+
+# GLOBAL ------------------------------------------------------------------
+
+path_dp <- "Data/Data Pack_Tanzania_Apr2821-PMTCT Test DP V2 1158 am.xlsx" 
+  
 # IMPORT ------------------------------------------------------------------
   
 #read in data pack
-  df_dp <- read_excel("Data/Data Pack_Tanzania_Apr2021-FY21 TX Test(v4)-1700.xlsx",
+  df_dp <- read_excel(path_dp,
                       sheet = "HTS",
                       skip = 13) 
   
@@ -35,18 +40,22 @@
   
 #flag wher ethere are issues and create the new share (cannot resolve where Other PITC < diff)
   df_fix <- df_fix %>%
-    mutate(is_off = HTS_TST.Pos.Diff != 0,
+    mutate(across(starts_with("HTS"), ~ ifelse(is.na(.), 0, .)),
+           is_off = HTS_TST.Pos.Diff != 0,
            is_resolved_now = case_when(is_off ~ HTS_TST.Other.Pos.T >= HTS_TST.Pos.Diff),
            new_other_pos = case_when(is_off ~ HTS_TST.Other.Pos.T - HTS_TST.Pos.Diff),
            new_other_pos_share = ifelse(is_off & is_resolved_now, new_other_pos/HTS_TST.Pos.T,
                                     HTS_TST.Other.Pos.Share))
 #export
-  df_fix1 %>% 
-    select(SNU1:ID, HTS_TST.Other.Pos.Share, is_off,is_resolved_now, new_other_pos_share) %>% 
-    write_csv("Dataout/fix1.csv", na = "")  
+  df_fix %>%
+    mutate(dp_row = row_number() +14, .before =1) %>% 
+    select(dp_row:ID, HTS_TST.Other.Pos.Share, is_off,is_resolved_now, new_other_pos_share) %>% 
+    write_csv("Dataout/hts_realignment.csv", na = "")  
   
 #review
-  df_fix1 %>% 
+  df_fix %>% 
     filter(is_off) %>% 
-    count(is_ok)
+    count(is_resolved_now)
+  
+  janitor::tabyl(df_fix, is_off, is_resolved_now) %>% janitor::adorn_totals(c("row", "col"))
   
