618df33359d67f99fb586df436885ebdfb8ab87a
Aaron Chafetz <achafetz@usaid.gov>
2022-06-28 16:25:41 -0400

    update for COP22

--- Scripts/COP21_USAID_IM-Workplan-targets.R
+++ Scripts/COP21_USAID_IM-Workplan-targets.R
@@ -3,14 +3,14 @@
 # PURPOSE:  COP21 Target for USAID workplans
 # LICENSE:  MIT
 # DATE:     2021-06-11
-# UPDATED:  2021-06-30
+# UPDATED:  2022-06-28
 
 # DEPENDENCIES ------------------------------------------------------------
   
   library(tidyverse)
   library(glitr)
   library(glamr)
-  library(ICPIutilities)
+  library(gophr)
   library(extrafont)
   library(scales)
   library(tidytext)
@@ -25,69 +25,93 @@
   load_secrets()
 
   #global material folder
-  glbl_id <- "1LSqjgZlhl28yaJpsBtZFSR2ME3eqB3Rr"
+  glbl_id <- "1aA71E4BET48-XF8qNG8u9KA1vS2s1GEw"
   
   #upload folder
   fldr_id <- "1JbZnP2k5OdIf_R6VCQ5fzTfOFFzvSjyU"
   
+  #COP FY
+  fy <- 2023
+  cop_yr <- fy - 1 - 2000
+  
 # FUNCTION ----------------------------------------------------------------
 
-  print_targets <- function(mech){
+  print_targets <- function(mech, cntry, output_folder){
     df_mech <- df_totals %>% 
-      filter(mech_code == mech)
+      filter(mech_code == mech,
+             country == cntry,
+             `2023` > 0)
     
     meta <- df_mech %>% 
       distinct(country, mech_code) %>%
       mutate(country = str_remove_all(country, " |'"),
-             name = glue("COP21IMTargets/COP21_{country}_{mech_code}_Targets.csv"))
+             name = glue("COP{cop_yr}_Targets_{country}_{mech_code}.csv"))
     
     print(glue("Printing...{meta$country}-{meta$mech_code}"))
     
-    write_csv(df_mech, file.path(meta$name), na = "")
+    write_csv(df_mech, file.path(output_folder, meta$name), na = "")
   }
 
 # IMPORT ------------------------------------------------------------------
   
-  #compiled data packs by ER team
-  #https://drive.google.com/drive/u/0/folders/1CD3Asd5Uror4YEGUpcXzv-ah0_UV3dkU
-  df_dp <- read_csv("../../../Downloads/Datapack_Master_06_09.csv")
+  #MSD as of FY22Q2c included COP22/FY23 targets
+  df_msd <- si_path() %>% 
+    return_latest("OU_IM") %>% 
+    read_msd()   
+  
 
 # MUNGE -------------------------------------------------------------------
 
-  #agregate totals
-  df_totals <- df_dp %>% 
-    filter(!(indicator %in% c("HTS_RECENT", "HTS_TST", "HTS_TST_POS", "PrEP_NEW", "PrEP_CURR",
-                              "TX_CURR", "TX_NEW", "TX_PVLS") & disagg == "KeyPop"),
-           fundingagency == "USAID") %>%
+  #available indicators with targets
+  inds <- df_msd %>% 
+    filter(fiscal_year == fy,
+           funding_agency == "USAID") %>%
+    pluck_totals() %>% 
+    distinct(indicator) %>% 
+    pull()
+  
+  #aggregate totals
+  df_totals <- df_msd %>% 
+    filter(funding_agency == "USAID",
+           indicator %in% inds) %>%
+    pluck_totals() %>% 
     clean_indicator() %>% 
-    mutate(country = ifelse(operatingunit == countryname, operatingunit, glue("{operatingunit}-{countryname}"))) %>% 
-    group_by(country, psnu, mech_code, mech_name, primepartner, fiscal_year, indicator) %>% 
-    summarise(targets = sum(targets, na.rm = TRUE)) %>% 
-    ungroup() %>% 
-    arrange(country, mech_code, indicator, psnu)
+    mutate(country = ifelse(operatingunit == country, operatingunit, glue("{operatingunit}-{country}"))) %>% 
+    count(country, mech_code, mech_name, prime_partner_name, fiscal_year, indicator, wt = targets, name = "targets") %>% 
+    arrange(country, mech_code, indicator) %>% 
+    mutate(targets = na_if(targets, 0)) %>% 
+    pivot_wider(names_from = fiscal_year,
+                values_from = targets)
 
 # CREATE TARGET FILES -----------------------------------------------------
 
   #list of mechanism
-  mechs <- df_totals %>% 
-    distinct(mech_code) %>% 
-    pull()
+  mechs <- df_msd %>% 
+    filter(fiscal_year == fy,
+           funding_agency == "USAID") %>%
+    pluck_totals() %>% 
+    distinct(mech_code, operatingunit, country) %>%
+    mutate(country = ifelse(operatingunit == country, operatingunit, glue("{operatingunit}-{country}"))) %>% 
+    select(mech_code, country)
   
-  #create folder for storing IM target files  
-  dir_create("COP21IMTargets")
+  #create folder for storing IM target files
+  temp_folder()
   
   #create IM target files
-  walk(mechs, print_targets)
+  mechs %>%
+    pwalk(~print_targets(..1, ..2, folderpath_tmp))
   
 
 # MOVE TO DRIVE -----------------------------------------------------------
 
   #create folder for upload
-  drive_mkdir("Test",
+  drive_mkdir(glue("COP{cop_yr} Mech Target Tables"),
               path = as_id(glbl_id))
   
+  fldr_id <- drive_ls(path = as_id(glbl_id), glue("COP{cop_yr} Mech Target Tables"))$id
+  
   #identify list of   
-  local_files <- list.files("COP21IMTargets", full.names = TRUE)
+  local_files <- list.files(folderpath_tmp, full.names = TRUE)
   
   #push to drive
   walk(local_files,
@@ -96,7 +120,7 @@
                       name = basename(.x),
                       type = "spreadsheet"))
   
-  #remove all local files
-  unlink("COP21IMTargets", recursive = TRUE)
-  
+  #share folder
+  drive_share(as_id(fldr_id), role = "reader",
+              type = "domain", domain = "usaid.gov")
   
